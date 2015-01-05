7f599ed7120bfc87f27e042f42861219cfc4ccaa
superuser <superuser@xinyunlian06>
2015-01-05 09:06:30 +0800

    conflict

--- public/js/jquery.pagination.js
+++ public/js/jquery.pagination.js
@@ -94,7 +94,6 @@
 		this.options = $.extend({},Pagination.defaults,options);
 		this.params  = $.extend({},Pagination.defaults.params,options.params);
 		this.isFetching = false ;
-    this.page = $.extend({},Pagination.page,options.page);
 	    this.page = $.extend({},Pagination.page,options.page);
 		this.$el.on('click','.page-prev:not(.disabled)',function(e){
 			me.prev();
@@ -102,9 +101,30 @@
 		this.$el.on('click','.page-next:not(.disabled)',function(e){
 			me.next();
 		});
-		this.$el.on('click','.page-number',function(e){
-			var $this = $(e.target);
-			me.onClick($this);
+	    this.$el.on('click','.page-first:not(.disabled)',function(e){
+			me.first();
+		});
+	    this.$el.on('click','.page-last:not(.disabled)',function(e){
+			me.last();
+		});
+	    this.$el.on('click','.page-skip input[type="button"]',function(e){
+	      var $this = $(this);
+	      var skip = $this.siblings('input[type="text"]').val().trim().toLowerCase();
+	      if(!isNaN(skip) && parseInt(skip)>=1 && parseInt(skip)<=me.options.totalPage){
+	        me.skip(parseInt(skip));
+	      }
+	    });
+	    this.$el.on('click','.page-number',function(e){
+			var $this = $(e.currentTarget);
+	        var val = $this.find('a').text().trim();
+	        if(!isNaN(val)){
+	          me.onClick($this);
+	        }else{
+	          var skip = parseInt(me.$el.find('.page-number.current a').text().trim());
+	          if((!(skip%3) || skip>=3) && skip<me.options.totalPage ){
+	            me.skip(skip+1);
+	          }   
+	        }
 		});
 	};
 	
@@ -140,155 +160,6 @@
 	
 	//Pagination.prototype.constructor = Pagination;
 	
-  Pagination.prototype.init = function(params){
-			var me = this;
-     var params = $.extend({},me.params,params); 
-			me.fetch(params);
-			var pageHtml = '<nav class="jq-pagination"><li class="page-first disabled"><a href="javascript:;">'+me.page[me.options.language].first+'</a></li>'
-						   +'<li class="page-prev disabled"><a href="javascript:;">'+me.page[me.options.language].prev+'</a></li>'	
-						   +'<li class="page-number current"><a href="javascript:;">'+1+'</a></li>'
-						   +'<li class="page-next disabled"><a href="javascript:;">'+me.page[me.options.language].next+'</a></li>'
-						   +'<li class="page-last disabled"><a href="javascript:;">'+me.page[me.options.language].last+'</a></li>'
-						   +'<li class="page-skip">skip to <input type="text" value="1"/><input type="button" value="'+me.page[me.options.language].skip+'"/></li></nav>';
-			var append ;
-			if(me.options.totalPage<=3){			
-				me.options.totalPage<3?(append='<li class="page-number"><a href="javascript:;">'+2+'</a>'):(append+='<li class="page-number"><a href="javascript:;">'+2+'</a></li>'+
-						'<li class="page-number"><a href="javascript:;">'+3+'</a></li>');				
-       $(pageHtml).find('.current').after(append).end()
-				.find('.page-next,.page-last').removeClass('disabled');
-			}
-			if(me.options.totalPage>3){
-				append = '<li class="page-number"><a href="javascript:;">'+2+'</a></li>'
-							 +'<li class="page-number"><a href="javascript:;">'+3+'</a></li>'
-							 +'<li class="page-number"><a href="javascript:;">'+"..."+'</a></li>'
-							 +'<li class="page-number"><a href="javascript:;">'+me.options.totalPage+'</a></li>';
-				$(pageHtml).find('.current').after(append).end()
-				.find('.page-next,.page-last').removeClass('disabled');
-			}
-     me.$el.append(pageHtml);
-			if(append){
-       $(append).insertAfter($('.current')); 
-       me.$el.find('.page-next,.page-last').removeClass('disabled');
-     }			
-		}
-  
-  Pagination.prototype.fetch = function(params){
-			var me = this;
-			//this.isFetching = true;
-			this.request = $.ajax({
-				url  : this.options.url,
-				data : { params : params },
-				type : 'GET',
-				dataType : 'json',
-				success  : function(result){
-					/**
-					 * result
-					 * @params totalPage 
-					 * @params content //if you need
-					 * 
-					 * */
-					var content ;
-					me.options.totalPage = result.totalPage || 3;
-					// content = result.content ;
-					me.$el.find('.page-prev,.page-first')[(params.pageNumber>1?'removeClass':(params.pageNumber==me.options.totalPage?'addClass':'removeClass'))]('disabled');
-					me.$el.find('.page-next,.page-last')[(params.pageNumber<me.options.totalPage?'removeClass':'addClass')]('disabled');
-					me.options.onSuccess.apply(me,[content]);
-				},
-				errot    : function(result){
-					
-				},
-				done     : function(result){
-					me.fetching = false;
-				}
-			});
-		}
-    
-  Pagination.prototype.prev =  function(){
-			if(this.isFetching){
-				return false;
-			}
-			if(this.options.params.pageNumber>1){
-				this.options.params.pageNumber -= 1;
-				this.$el.find('.current').removeClass('current').prev('.page-number').addClass('current').end()
-        .end().find('.page-next,.page-last').removeClass('disabled');       
-				this.fetch(this.options.params);
-        if(this.options.params.pageNumber==1){
-        this.$el.find('.page-prev,.page-first').addClass('disabled');
-       }
-			}
-		}
-
-  Pagination.prototype.next =  function(){
-			if(this.isFetching){
-				return false;
-			}
-			if(this.options.params.pageNumber<this.options.totalPage){
-				this.options.params.pageNumber += 1;
-				this.$el.find('.current').removeClass('current').next('.page-number').addClass('current').end()
-        .end().find('.page-prev,.page-first').removeClass('disabled'); 
-				this.fetch(this.options.params);
-       if(this.options.params.pageNumber==this.options.totalPage){
-        this.$el.find('.page-next,.page-last').addClass('disabled');
-       }  
-			}
-		}
-    
-  Pagination.prototype.first = function(){
-    if(this.isFetching){
-				return false;
-			}
-    this.options.params.pageNumber = 1; 
-    this.$el.find('.current').removeClass('current').end().find('.page-number').eq(0).addClass('current').end()
-        .end().find('.page-prev,.page-first').addClass('disabled').end().find('.page-next,.page-last').removeClass('disabled');
-		this.fetch(this.options.params);  
-  }  
-
-  Pagination.prototype.last = function(){
-    if(this.isFetching){
-				return false;
-			}
-    this.options.params.pageNumber = this.options.totalPage; 
-    this.$el.find('.current').removeClass('current').end().find('.page-number').eq(this.options.totalPage-1).addClass('current').end()
-        .end().find('.page-next,.page-last').addClass('disabled').end().find('.page-prev,.page-first').removeClass('disabled');
-		this.fetch(this.options.params);  
-  }
-  
-  Pagination.prototype.onClick =  function($obj){
-			if(this.isFetching){
-				return false;
-			}
-			$obj.parent().addClass('current').siblings('.page-number').removeClass('current');
-			this.fetch($.extend({},this.options.params,{
-				pageNumber:$obj.text().trim().toLowerCase()
-			}));
-		}   
-  
-	/*Pagination.prototype = {
-		init  : function(params){
-			var me = this;
-     var params = $.extend({},me.params,params); 
-			me.fetch(params);
-			var pageHtml = '<nav><li class="page-first disabled"><a href="javascript:;">'+me.page[me.options.language].first+'</a></li>'
-						   +'<li class="page-prev disabled"><a href="javascript;;">'+me.page[me.options.language].prev+'</a></li>'	
-						   +'<li class="page-number current"><a href="javascript:;">'+1+'</a></li>'
-						   +'<li class="page-next disabled"><a href="javascript:;">'+me.page[me.options.language].next+'</a></li>'
-						   +'<li class="page-last disabled"><a href="javascript:;">'+me.page[me.options.language].last+'</a></li>'
-						   +'<li class="page-skip disabled">skip to <input type="text" value="1"/><input type="button" value="'+me.page[me.options.language].skip+'"/></li></nav>';
-			var append ;
-			if(me.options.totalPage<=3){			
-				me.options.totalPage<3?(append='<li class="page-number"><a href="javascript:;">'+2+'</a>'):(append+='<li class="page-number"><a href="javascript:;">'+2+'</a></li>'+
-						'<li class="page-number"><a href="javascript:;">'+3+'</a></li>');				
-       $(pageHtml).find('.current').after(append).end()
-				.find('.page-next,.page-last').removeClass('disabled');
-			}
-			if(me.options.totalPage>3){
-				append = '<li class="page-number"><a href="javascript:;">'+2+'</a></li>'
-							 +'<li class="page-number"><a href="javascript:;">'+3+'</a></li>'
-							 +'<li class="page-number"><a href="javascript:;">'+"..."+'</a></li>'
-							 +'<li class="page-number"><a href="javascript:;">'+me.options.totalPage+'</a></li>';
-				$(pageHtml).find('.current').after(append).end()
-				.find('.page-next,.page-last').removeClass('disabled');
-			}
   Pagination.prototype.init = function(params){
 	 var me = this;
      var params = $.extend({},me.params,params); 
@@ -345,63 +216,7 @@
 		},
 		errot    : function(result){
 			
-			me.$el.append(pageHtml);
-     $(append).insertAfter($('.current')); 
-		},	
-		fetch : function(params){
-			var me = this;
-			this.isFetching = true;
-			this.request = $.ajax({
-				url  : this.options.url,
-				data : { params : params },
-				type : 'GET',
-				dataType : 'json',
-				success  : function(result){
-					
-					var content ;
-					me.options.totalPage = result.totalPage || 2;
-					// content = result.content ;
-					me.$el.find('.page-prev,.page-first')[(params.pageNumber>1?'removeClass':(params.pageNumber==me.options.totalPage?'addClass':'removeClass'))]('.disabled');
-					me.$el.find('.page-next,.page-last')[(params.pageNumber<me.options.totalPage?'removeClass':'addClass')]('.disabled');
-					me.options.onSuccess.apply(me,[content]);
-				},
-				errot    : function(result){
-					
-				},
-				done     : function(result){
-					me.fetching = false;
-				}
-			});
 		},
-		next : function(){
-			if(this.isFetching){
-				return false;
-			}
-			if(this.options.pageNumber<this.options.totalPage){
-				this.options.params.pageNumber += 1;
-				this.$el.find('.current').removeClass('.current').next('.page-number').addClass('.current');
-				this.fetch(this.options.params);
-			}
-		},
-		prev : function(){
-			if(this.isFetching){
-				return false;
-			}
-			if(this.options.pageNumber>1){
-				this.options.params.pageNumber -= 1;
-				this.$el.find('.current').removeClass('.current').prev('.page-number').addClass('.current');
-				this.fetch(this.options.params);
-			}
-		},
-		onClick : function($obj){
-			if(this.isFetching){
-				return false;
-			}
-     this.options.params.pageNumber = $obj.text().trim().toLowerCase();
-			$obj.parent().addClass('.current').siblings('.page-number').removeClass('.current');
-			this.fetch($.extend({},this.options.params,{
-				pageNumber:$obj.find('a').text().trim()
-			}));
 		done     : function(result){
 			me.fetching = false;
 		}
@@ -421,9 +236,6 @@
 		if(this.options.params.pageNumber==1){
 		this.$el.find('.page-prev,.page-first').addClass('disabled');
 		}
-	};
-	*/
-	$.fn.pagination = function(options,args){
 	  }
 	}
 
@@ -524,9 +336,12 @@
 			data = $this.data('pagination');
 			if(!data){
 				$this.data('pagination',new Pagination(this,options));
-			}else if(typeof options==='string'){
-				data[options](args);
-			}
+			   if(typeof args==='string'){
+			     $this.data('pagination')[args](options.params);
+			   	}
+		}else if(typeof args==='string'){
+			data[args](options);
+		}
 		});
 	};
 	/*var options= {
@@ -538,3 +353,4 @@
       };
     $('.test-pagination').pagination('init',options);*/
 }));
+
