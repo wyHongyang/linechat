d70cd458402b5cf9a30c3a1cd94161aa7aacf9a0
superuser <superuser@xinyunlian06>
2015-01-05 17:14:44 +0800

    add validator

--- public/js/jquery.validator.js
+++ public/js/jquery.validator.js
@@ -15,6 +15,22 @@
 			return this.each(function(){
 				
 			});
+			
+			/**
+			 * 只允许输入数字
+			 * */
+			
+			var conflict = funciton(event){
+				var $target = $(event.target);
+				var keyCode = event.keyCode;
+				if($target[0].localName === 'input' && $target[0].type == 'text'){
+					if(keyCode>=48 && keyCode<=57){
+						event.returnvalue = true; 
+					}esle{
+						event.returnvalue = false;
+					}
+				}
+			};
 		}
 	});
 })(jQuery);
